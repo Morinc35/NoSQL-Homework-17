eca10967286b1f170a8b3acde33e1a1146b46259
Cameron Morin <morinc35@gmail.com>
2023-10-07 13:26:51 -0400

    Basic file structure and connection setup

---
+++ controllers/userController.js
@@ -0,0 +1,1 @@
+const { User, Application } = require('../models');
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const db =require('./config/connection')
+
+const app = express();
+const port = 3001;
+
+
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('01-Activities')[1]
+  : cwd;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+db.once('open', () => {
+    app.listen(port, () => {
+      console.log(`API server for ${activity} running on port ${port}!`);
+    });
+  });
